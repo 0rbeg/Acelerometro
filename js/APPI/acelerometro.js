cedff4d1af7f9ed246db12bb2b1e414b5bfdbbab
0rbeg <gortegab@gmail.com>
2015-11-28 10:26:04 -0600

    1

---
+++ js/APPI/acelerometro.js
@@ -0,0 +1,22 @@
+var ac = {
+    watchID: null,
+    options: {frecuency:500},
+    init: function(){
+//        alert("Iniciar");
+        if(!ac.watchID)
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.Success, ac.error, ac.options);
+    },
+    stop: function(){
+        //alert("detener");
+        if(ac.watchID != null)
+			navigator.accelerometer.clearWatch(ac.watchID);
+    },
+    success: function(a){
+		$('#acelerometro .scroll h2').html('X: ' + a.x +
+										   'Y: ' + a.y +
+										   'Z: ' + a.z);
+	},
+	error: function(err){
+		alert(err.code);
+	}
+};
