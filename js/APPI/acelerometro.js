39df38e35fd04e2f242d5ddf546e707846797eb5
0rbeg <gortegab@gmail.com>
2015-11-28 11:02:07 -0600

    4

--- js/APPI/acelerometro.js
+++ js/APPI/acelerometro.js
@@ -4,12 +4,16 @@ var ac = {
     init: function(){
 //        alert("Iniciar");
         if(!ac.watchID)
-            ac.watchID = navigator.accelerometer.watchAcceleration(ac.Success, ac.error, ac.options);
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.uccess, ac.error, ac.options);
     },
     stop: function(){
         //alert("detener");
-        if(ac.watchID != null)
+        if(ac.watchID != null){
 			navigator.accelerometer.clearWatch(ac.watchID);
+            ac.watchID = null;
+            $('#acelerometro .scroll h2').html('Detenido');
+        }
+        
     },
     success: function(a){
 		$('#acelerometro .scroll h2').html('X: ' + a.x +
